0f8dfe34b378df31e63be94152f7b15d8a6908aa
jian.fbehind <jian.fbehind@gmail.com>
2024-01-06 15:44:04 +0000

    update group_op

--- eagleeye/engine/nano/op/group_op.h
+++ eagleeye/engine/nano/op/group_op.h
@@ -5,6 +5,7 @@
 #include "eagleeye/basic/Dim.h"
 #include "eagleeye/basic/type.h"
 #include "eagleeye/engine/nano/op/dynamiccreater.h"
+#include "eagleeye/common/"
 #include <string>
 #include <vector>
 
@@ -14,7 +15,8 @@ template<std::size_t IN, std::size_t OUT>
 class GroupOp:public BaseOp<IN,OUT>{
 public:
     using BaseOp<IN,OUT>::init; 
-    GroupOp(){}
+    GroupOp(){
+    }
     virtual ~GroupOp(){
         for(int op_i=0; op_i<m_ops.size(); ++op_i){
             delete m_ops[op_i];
@@ -23,27 +25,22 @@ public:
     }
 
     virtual int init(std::map<std::string, std::vector<float>> params){
-        for(int op_i=0; op_i<m_ops.size(); ++op_i){
-            m_ops[op_i]->init(params);
-        }
         return 0;
     };
     virtual int init(std::map<std::string, std::vector<std::vector<float>>> params){
-        for(int op_i=0; op_i<m_ops.size(); ++op_i){
-            m_ops[op_i]->init(params);
-        }
         return 0;
     };
     virtual int init(std::map<std::string, std::vector<std::string>> params){
+        // 内部算子链接关系
         for(int op_i=0; op_i<m_ops.size(); ++op_i){
-            m_ops[op_i]->init(params);
+            std::string op_i_name = std::to_string(op_i);
+            if(params.find(op_i_name) != params.end()){
+                m_relations.push_back(params[op_i_name]);
+            }
         }
         return 0;
     }
     virtual int init(std::map<std::string, void*> params){
-        for(int op_i=0; op_i<m_ops.size(); ++op_i){
-            m_ops[op_i]->init(params);
-        }
         return 0;
     }
 
@@ -53,20 +50,44 @@ public:
             return 0;
         }
 
-        int ops_num = this->m_ops.size();
-        this->m_ops[0]->runOnCpu(input);
-        for(int op_i=1; op_i<ops_num; ++op_i){
-            this->m_ops[op_i]->runOnCpu(this->m_ops[op_i-1]->m_outputs);
+        std::map<std::string, Tensor> inner_data_dict;
+        for(int op_i=0; op_i<m_ops.size(); ++op_i){
+            std::string input_info = m_relations[op_i][0];
+            std::string output_info = m_relations[op_i][1];
+
+            std::string separator = ",";
+            std::vector<std::string> input_terms = split(input_info, separator);
+            std::vector<std::string> output_terms = split(output_info, separator);
+
+            std::vector<Tensor> op_input;
+            for(int op_input_i=0; op_input_i<input_terms.size(); ++op_input_i){
+                std::string op_input_i_name = input_terms[op_input_i];
+                if(inner_data_dict.find(op_input_i_name) == inner_data_dict.ends()){
+                    op_input.push_back(input[tof<int>(op_input_i_name)]);
+                }
+                else{
+                    op_input.push_back(inner_data_dict[op_input_i_name]);
+                }
+            }
+
+            this->m_ops[op_i]->runOnCpu(op_input);
+
+            for(int op_output_i=0; op_output_i<output_terms.size(); ++op_output_i){
+                inner_data_dict[output_terms[op_output_i]] = this->m_ops[op_i].getOutput(op_output_i);
+            }
         }
 
-        this->m_outputs = this->m_ops[ops_num-1]->m_outputs;
+        for(int output_i=0; output_i<OUT; ++output_i){
+            this->m_outputs[std::to_string(output_i)] = inner_data_dict[std::to_string(output_i)];
+        }
+
         return 0;
     }
     virtual int runOnGpu(const std::vector<Tensor>& input){return 0;}
 
 protected:
     std::vector<Base*> m_ops;
-
+    std::vector<std::vector<std::string>> m_relations;
 };
 }
 }
