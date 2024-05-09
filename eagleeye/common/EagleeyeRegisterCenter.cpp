c5c90e5f20a0bc921e4d21b4ccb6d55d7bca916c
lichao111 <515359166@qq.com>
2024-05-09 05:44:05 +0000

    update: register center check timeout

--- eagleeye/common/EagleeyeRegisterCenter.cpp
+++ eagleeye/common/EagleeyeRegisterCenter.cpp
@@ -1,9 +1,15 @@
 #include "eagleeye/common/EagleeyeRegisterCenter.h"
 #include "eagleeye/common/EagleeyeStr.h"
+#include "eagleeye/common/EagleeyeLog.h"
 namespace eagleeye{
 std::shared_ptr<RegisterCenter> RegisterCenter::m_instance(new RegisterCenter(), [](RegisterCenter* d) { delete d; });    
 RegisterCenter::RegisterCenter(){}
-RegisterCenter::~RegisterCenter(){}
+RegisterCenter::~RegisterCenter(){
+    m_run_check = false;
+    if(m_future.valid()){
+        m_future.wait();
+    }
+}
 RegisterCenter* RegisterCenter::getInstance(){
     return m_instance.get();
 }
@@ -42,6 +48,7 @@ bool RegisterCenter::registerObj(std::string key, void* obj, std::function<void(
     }
 
     m_register_map[key] = std::make_pair(obj, destroy_func);
+    m_register_start_time[key] = std::chrono::steady_clock::now();
     return true;
 }
 
@@ -55,6 +62,7 @@ bool RegisterCenter::destroyObj(std::string key){
     void* obj = m_register_map[key].first;
     destroy_obj_func(key, obj);
     m_register_map.erase(key);
+    m_register_start_time.erase(key);
     return true;
 }
 
@@ -67,9 +75,40 @@ bool RegisterCenter::destroyObjWithPrefix(std::string prefix){
             void* obj = m_register_map[iter->first].first;
             destroy_obj_func(iter->first, obj);
             m_register_map.erase(iter->first);
+            m_register_start_time.erase(iter->first);
         }
     }
 
     return true;
+}
+
+void RegisterCenter::enableTimeout(int timeout_seconds){
+    m_run_check = true;
+    m_future = std::async(std::launch::async, &RegisterCenter::startTimerThread, this, timeout_seconds);
+}
+
+void RegisterCenter::startTimerThread(int timeout_seconds){
+    while(m_run_check == true){
+        std::this_thread::sleep_for(60s);
+        std::vector<std::string> keys_to_destroy;
+        {
+            std::lock_guard<std::mutex> lock(m_mu);
+            for(auto &[key, destroy_func] : m_register_map){
+                if(m_register_start_time.count(key) == 0){
+                    EAGLEEYE_LOGE("RegisterCenter found error key = [%s], it has no start time", key.c_str());
+                    keys_to_destroy.emplace_back(key);
+                }
+                if (std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - m_register_start_time[key]).count()> timeout_seconds) {
+                    keys_to_destroy.emplace_back(key);
+                } 
+            }
+        }
+        for(const auto& key: keys_to_destroy){
+            EAGLEEYE_LOGI("RegisterCenter found timeout key, now destroy key = [%s]", key.c_str());
+            if(not destroyObj(key)){
+                EAGLEEYE_LOGE("RegisterCenter destroy key = [%s] failed", key.c_str());
+            }
+        }
+    }
 }
 }
