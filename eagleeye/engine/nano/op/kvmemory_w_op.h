017042758680e080a199edd9370e1c0b8c97af9d
jian.fbehind <jian.fbehind@gmail.com>
2024-05-09 06:57:15 +0000

    fix faceid

--- eagleeye/engine/nano/op/kvmemory_w_op.h
+++ eagleeye/engine/nano/op/kvmemory_w_op.h
@@ -9,9 +9,9 @@
 
 namespace eagleeye{
 namespace dataflow{
-class KVMemoryWOp:public BaseOp<3, 1>, DynamicCreator<KVMemoryWOp>{
+class KVMemoryWOp:public BaseOp<4, 1>, DynamicCreator<KVMemoryWOp>{
 public:
-    using BaseOp<3, 1>::init;
+    using BaseOp<4, 1>::init;
     KVMemoryWOp();
     KVMemoryWOp(const KVMemoryWOp& op);
     virtual ~KVMemoryWOp();
@@ -23,18 +23,9 @@ public:
     virtual int runOnCpu(const std::vector<Tensor>& input);
     virtual int runOnGpu(const std::vector<Tensor>& input);
 
-public:
-    // key: (16(ID)+16(扩展编码))
-    static std::map<std::string, std::map<std::string, Tensor>> m_g_memory;
-    static std::map<std::string, std::map<std::string, std::vector<std::string>>> m_g_info;
-
 protected:
-    std::string m_memory_name;
     std::string m_cache_folder;
     std::string m_cache_memory_folder;
-
-    bool m_is_init;
-    bool m_enable_group;
 };
 }
 }
